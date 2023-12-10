7936afd847ff8296269bc9d04f364d39473bb7e5
Sabin <spradh@users.noreply.github.com>
2023-12-09 22:07:10 -0600

    Update 6-Adding-error-bars-to-a-plot.py

--- 3-Quantitative-comparisons-and-statistical-visualization/6-Adding-error-bars-to-a-plot.py
+++ 3-Quantitative-comparisons-and-statistical-visualization/6-Adding-error-bars-to-a-plot.py
@@ -1,1 +1,13 @@
+fig, ax = plt.subplots()
 
+# Add Seattle temperature data in each month with error bars
+ax.errorbar(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"], seattle_weather["MLY-TAVG-STDDEV"])
+
+# Add Austin temperature data in each month with error bars
+ax.errorbar(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"], austin_weather["MLY-TAVG-STDDEV"])
+ 
+
+# Set the y-axis label
+ax.set_ylabel("Temperature (Fahrenheit)")
+
+plt.show()
