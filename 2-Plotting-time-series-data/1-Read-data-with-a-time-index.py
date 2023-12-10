c5fdcadfaa4d238750b78d35a039b50dea20db18
Sabin <spradh@users.noreply.github.com>
2023-12-09 19:28:57 -0600

    Update 1-Read-data-with-a-time-index.py

--- 2-Plotting-time-series-data/1-Read-data-with-a-time-index.py
+++ 2-Plotting-time-series-data/1-Read-data-with-a-time-index.py
@@ -1,1 +1,5 @@
-#TODO
+# Import pandas as pd
+import pandas as pd
+
+# Read the data from file using read_csv
+climate_change = pd.read_csv('climate_change.csv', parse_dates = ["date"], index_col = "date")
